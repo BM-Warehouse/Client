32c5aac2515c369c8e91cbcd226517f0b7ab8ba2
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-05-07 20:44:37 +0700

    feat: add sidebar and navbar

---
+++ src/app/products/page.js
@@ -0,0 +1,20 @@
+// import { HiOutlineUsers } from 'react-icons/hi2';
+// import { LuBox, LuWarehouse, LuBoxes } from 'react-icons/lu';
+// import { MdOutlineDashboard } from 'react-icons/md';
+// import { TbReceipt2 } from 'react-icons/tb';
+
+// import Sidebar, { SidebarItem } from '@/components/parts/Sidebar';
+
+import Navbar from '@/components/parts/Navbar';
+import Sidebar from '@/components/parts/Sidebar';
+
+function Products() {
+  return (
+    <main className="App">
+      <Navbar />
+      <Sidebar />
+    </main>
+  );
+}
+
+export default Products;
---
+++ src/components/parts/Sidebar/index.jsx
@@ -0,0 +1,21 @@
+import { HiOutlineUsers } from 'react-icons/hi2';
+import { LuBox, LuWarehouse, LuBoxes } from 'react-icons/lu';
+import { MdOutlineDashboard } from 'react-icons/md';
+import { TbReceipt2 } from 'react-icons/tb';
+
+import SidebarElem, { SidebarItem } from '@/components/elements/SidebarElem';
+
+function Sidebar() {
+  return (
+    <SidebarElem>
+      <SidebarItem icon={<MdOutlineDashboard className="text-xl" />} text="Dashboard" active />
+      <SidebarItem icon={<LuBox className="text-xl" />} text="Products" />
+      <SidebarItem icon={<LuBoxes lassName="text-xl" />} text="Categories" />
+      <SidebarItem icon={<LuWarehouse className="text-xl" />} text="Warehouses" />
+      <SidebarItem icon={<TbReceipt2 className="text-xl" />} text="Orders" alert />
+      <SidebarItem icon={<HiOutlineUsers className="text-xl" />} text="Management Users" />
+    </SidebarElem>
+  );
+}
+
+export default Sidebar;
