b6009776cb7ddccb28554015a958929d07f2e90c
Febrio Evan Hartanto <febrioevanhartanto@gmail.com>
2024-05-13 05:27:00 +0800

    fix: fixed code linting

--- src/hooks/useModalStore.js
+++ src/hooks/useModalStore.js
@@ -1,10 +1,9 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
 const useStoreModal = create((set) => ({
   isOpen: false,
-  openModal : () => set({isOpen: true}),
-  closeModal : () => set({isOpen: false})
-})
-);
+  openModal: () => set({ isOpen: true }),
+  closeModal: () => set({ isOpen: false })
+}));
 
 export default useStoreModal;
