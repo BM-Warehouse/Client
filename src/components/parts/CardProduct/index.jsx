801600505201075741c137ff03390c2f0bbab905
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-05-13 20:22:13 +0700

    fix: list product page

--- src/components/parts/CardProduct/index.jsx
+++ src/components/parts/CardProduct/index.jsx
@@ -11,7 +11,7 @@ function CardProduct({ product }) {
   return (
     <div className="w-30 card mb-10 cursor-pointer bg-base-100 shadow-xl hover:drop-shadow-2xl xl:w-60">
       <figure>
-        <img src={product.imageUrl} alt="Shoes" />
+        <img src={product.imageUrl} alt={product.name} />
       </figure>
       <div className="card-body gap-0 px-6 py-2">
         <h2 className="card-title text-sm md:text-base">{product.name}</h2>
