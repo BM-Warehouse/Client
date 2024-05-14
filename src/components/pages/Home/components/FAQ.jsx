0504c6f6ff0d680f183b7c3b508421c3178421b8
Yuma Nur Alfath <108206424+yumanuralfath@users.noreply.github.com>
2024-05-14 11:49:19 +0700

    fix: fix footer and faq section

--- src/components/pages/Home/components/FAQ.jsx
+++ src/components/pages/Home/components/FAQ.jsx
@@ -5,52 +5,72 @@ import React from 'react';
 const FAQ = () => (
   <div className="mt-10 flex w-full flex-col items-center">
     <h2 className="text-3xl font-bold text-secondary">FAQ</h2>
-    <div className="faq-content m-10 w-full rounded-xl bg-secondary p-5 md:px-10">
-      <details>
-        <summary className="cursor-pointer py-3 text-white">Apakah ini pertanyaan?</summary>
-        <div className="faq-content-answer px-5 py-3 text-white">
+    <div className="faq content mt-10 w-full px-5 xl:px-20 ">
+      <div className="collapse mb-5 w-full rounded-sm border border-tertiary">
+        <input type="checkbox" />
+        <div className="text-md collapse-title font-medium text-tertiary md:text-lg xl:text-xl">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
+        </div>
+        <div className="collapse-content text-xs md:text-sm xl:text-base">
           <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-            tincidunt ut laoreet dolore.
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem
+            ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem ipsum
+            dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
           </p>
         </div>
-      </details>
-      <details>
-        <summary className="cursor-pointer py-3 text-white">Apakah ini pertanyaan?</summary>
-        <div className="faq-content-answer px-5 py-3 text-white">
+      </div>
+      <div className="collapse mb-5 w-full rounded-sm border border-tertiary">
+        <input type="checkbox" />
+        <div className="text-md collapse-title font-medium text-tertiary md:text-lg xl:text-xl">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
+        </div>
+        <div className="collapse-content text-xs md:text-sm xl:text-base">
           <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-            tincidunt ut laoreet dolore.
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem
+            ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem ipsum
+            dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
           </p>
         </div>
-      </details>
-      <details>
-        <summary className="cursor-pointer py-3 text-white">Apakah ini pertanyaan?</summary>
-        <div className="faq-content-answer px-5 py-3 text-white">
+      </div>
+      <div className="collapse mb-5 w-full rounded-sm border border-tertiary">
+        <input type="checkbox" />
+        <div className="text-md collapse-title font-medium text-tertiary md:text-lg xl:text-xl">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
+        </div>
+        <div className="collapse-content text-xs md:text-sm xl:text-base">
           <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-            tincidunt ut laoreet dolore.
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem
+            ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem ipsum
+            dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
           </p>
         </div>
-      </details>
-      <details>
-        <summary className="cursor-pointer py-3 text-white">Apakah ini pertanyaan?</summary>
-        <div className="faq-content-answer px-5 py-3 text-white">
+      </div>
+      <div className="collapse mb-5 w-full rounded-sm border border-tertiary">
+        <input type="checkbox" />
+        <div className="text-md collapse-title font-medium text-tertiary md:text-lg xl:text-xl">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
+        </div>
+        <div className="collapse-content text-xs md:text-sm xl:text-base">
           <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-            tincidunt ut laoreet dolore.
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem
+            ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem ipsum
+            dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
           </p>
         </div>
-      </details>
-      <details>
-        <summary className="cursor-pointer py-3 text-white">Apakah ini pertanyaan?</summary>
-        <div className="faq-content-answer px-5 py-3 text-white">
+      </div>
+      <div className="collapse mb-5 w-full rounded-sm border border-tertiary">
+        <input type="checkbox" />
+        <div className="text-md collapse-title font-medium text-tertiary md:text-lg xl:text-xl">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
+        </div>
+        <div className="collapse-content text-xs md:text-sm xl:text-base">
           <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-            tincidunt ut laoreet dolore.
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem
+            ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?Lorem ipsum
+            dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh?
           </p>
         </div>
-      </details>
+      </div>
     </div>
   </div>
 );
