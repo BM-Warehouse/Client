d602512c34388493fad49eb457fad695d9be50fd
Yohan Prakoso <prakoso.yohan@gmail.com>
2024-05-15 16:30:32 +0800

    feat: dynamic active berdasarkan routePath

--- src/components/parts/Sidebar/index.jsx
+++ src/components/parts/Sidebar/index.jsx
@@ -1,15 +1,28 @@
+'use client';
 import { HiOutlineUsers } from 'react-icons/hi2';
 import { LuBox, LuWarehouse, LuBoxes } from 'react-icons/lu';
 import { MdOutlineDashboard } from 'react-icons/md';
 import { TbReceipt2 } from 'react-icons/tb';
 
 import SidebarElem, { SidebarItem } from '@/components/elements/SidebarElem';
+import { usePathname } from 'next/navigation';
 
 function Sidebar() {
+  const routePath = usePathname();
   return (
     <SidebarElem>
-      <SidebarItem icon={<MdOutlineDashboard />} text="Dashboard" href="/test" active />
-      <SidebarItem icon={<LuBox />} text="Products" href="/products" />
+      <SidebarItem
+        icon={<MdOutlineDashboard />}
+        text="Dashboard"
+        href="/test"
+        {...(routePath === '/test' && { active: true })}
+      />
+      <SidebarItem
+        icon={<LuBox />}
+        text="Products"
+        href="/products"
+        {...(routePath === '/products' && { active: true })}
+      />
       <SidebarItem icon={<LuBoxes />} text="Categories" />
       <SidebarItem icon={<LuWarehouse />} text="Warehouses" />
       <SidebarItem icon={<TbReceipt2 />} text="Orders" alert />
