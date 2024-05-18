f12d787f100cfde2010a0f09d18513c1eb2c14fc
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-05-18 15:43:32 +0700

    feat: add functional move stock

--- src/components/parts/ContainerProductsAdmin/index.jsx
+++ src/components/parts/ContainerProductsAdmin/index.jsx
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from 'react';
 
 import ModalAddStockProduct from '@/components/parts/ModalAddStockProduct';
+import ModalMoveStockProduct from '@/components/parts/ModalMoveStockProduct';
 import RowProduct from '@/components/parts/RowProduct';
 import useWarehouseStore from '@/store/warehouseStore';
 
 function ContainerProductsAdmin({ productsData }) {
   const { warehouseData, getWarehouseData } = useWarehouseStore();
   const [selectedProduct, setSelectedProduct] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddStockModalOpen, setIsAddStockModalOpen] = useState(false);
+  const [isMoveStockModalOpen, setIsMoveStockModalOpen] = useState(false);
 
-  const handleOpenModal = (product) => {
+  const handleOpenAddStockModal = (product) => {
     setSelectedProduct(product);
-    setIsModalOpen(true);
+    setIsAddStockModalOpen(true);
   };
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
+  const handleCloseAddStockModal = () => {
+    setIsAddStockModalOpen(false);
+    setSelectedProduct(null);
+  };
+
+  const handleOpenMoveStockModal = (product) => {
+    setSelectedProduct(product);
+    setIsMoveStockModalOpen(true);
+  };
+
+  const handleCloseMoveStockModal = () => {
+    setIsMoveStockModalOpen(false);
     setSelectedProduct(null);
   };
 
   useEffect(() => {
     getWarehouseData();
   }, [getWarehouseData]);
-
-  console.log(warehouseData);
 
   if (!productsData) {
     return <div>Loading...</div>;
@@ -44,15 +54,27 @@ function ContainerProductsAdmin({ productsData }) {
           </thead>
           <tbody className="text-tertiary">
             {productsData.map((product) => (
-              <RowProduct key={product.id} product={product} onOpenModal={handleOpenModal} />
+              <RowProduct
+                key={product.id}
+                product={product}
+                onOpenModal={handleOpenAddStockModal}
+                onOpenMoveModal={handleOpenMoveStockModal}
+              />
             ))}
           </tbody>
         </table>
       </div>
-      {isModalOpen && (
+      {isAddStockModalOpen && (
         <ModalAddStockProduct
           product={selectedProduct}
-          onClose={handleCloseModal}
+          onClose={handleCloseAddStockModal}
+          warehouseData={warehouseData}
+        />
+      )}
+      {isMoveStockModalOpen && (
+        <ModalMoveStockProduct
+          product={selectedProduct}
+          onClose={handleCloseMoveStockModal}
           warehouseData={warehouseData}
         />
       )}
--- src/components/parts/ModalMoveStockProduct/index.jsx
+++ src/components/parts/ModalMoveStockProduct/index.jsx
@@ -3,16 +3,33 @@ import { useEffect, useState } from 'react';
 import useProductStore from '@/store/productStore';
 import useWarehouseStore from '@/store/warehouseStore';
 
-function ModalMoveStockProduct({ product, onClose }) {
-  const { warehouseData, getWarehouseData } = useWarehouseStore();
-  const { asyncAddProductToWarehouse, asyncGetAll } = useProductStore();
-
-  const [selectedWarehouse, setSelectedWarehouse] = useState('');
+function ModalMoveStockProduct({ product, onClose, warehouseData }) {
+  const { asyncMoveProductWarehouse } = useProductStore();
+  const { getWarehouseData } = useWarehouseStore();
+  const [selectedOriWarehouse, setSelectedOriWarehouse] = useState('');
+  const [selectedDestWarehouse, setSelectedDestWarehouse] = useState('');
   const [stock, setStock] = useState('');
+  const [maxStock, setMaxStock] = useState(0);
+
+  // console.log(warehouseData);
 
   useEffect(() => {
-    getWarehouseData();
-  }, [getWarehouseData]);
+    if (selectedOriWarehouse) {
+      const selectedWarehouse = warehouseData.find((war) => war.id === +selectedOriWarehouse);
+
+      if (selectedWarehouse) {
+        const productStock = selectedWarehouse.productsWarehouses.find(
+          (pw) => pw.product.id === product.id
+        );
+
+        setMaxStock(productStock ? productStock.quantity : 0);
+      } else {
+        setMaxStock(0);
+      }
+    } else {
+      setMaxStock(null);
+    }
+  }, [selectedOriWarehouse, product, warehouseData]);
 
   if (!product) {
     return null;
@@ -21,41 +38,70 @@ function ModalMoveStockProduct({ product, onClose }) {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await asyncAddProductToWarehouse(product.id, +selectedWarehouse, +stock);
-      asyncGetAll();
+      await asyncMoveProductWarehouse(
+        product.id,
+        +selectedOriWarehouse,
+        +selectedDestWarehouse,
+        +stock
+      );
+      await getWarehouseData();
       onClose();
+
+      console.log(product.id, +selectedOriWarehouse, +selectedDestWarehouse, +stock);
     } catch (error) {
-      console.error('Error adding stock:', error.message);
+      console.error('Error moving stock:', error.message);
     }
   };
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <div className="absolute inset-0 bg-black opacity-50" />
-      <div className="relative z-10 rounded-lg bg-primary p-6">
+      <div className="absolute inset-0 bg-black opacity-50 " />
+      <div className="relative z-10 rounded-lg bg-primary p-6 w-[29rem]">
         <h3 className="text-lg font-bold text-secondary">Move Stock Product</h3>
         <form onSubmit={handleSubmit} className="input-container mt-4 text-sm">
           <div className="mb-3 flex items-center">
             <label htmlFor="name" className="w-5/12 text-secondary">
-              Product name :
+              Product name:
             </label>
             <input
               disabled
               type="text"
               defaultValue={product.name}
               name="name"
-              className="input max-h-10 w-7/12 border-secondary  px-4 py-0 text-sm placeholder:text-secondary"
+              className="input max-h-10 w-7/12 border-secondary px-4 py-0 text-sm placeholder:text-secondary"
             />
           </div>
-          <div className="mb-3 flex  items-center ">
+          <div className="mb-3 flex items-center">
             <label htmlFor="warehouse" className="w-5/12 text-secondary">
-              Warehouse :
+              Original Warehouse:
             </label>
             <select
               name="warehouse"
               className="select max-h-10 w-7/12 border border-tertiary bg-bgColor px-4 py-0 text-sm"
-              value={selectedWarehouse}
-              onChange={(e) => setSelectedWarehouse(e.target.value)}
+              value={selectedOriWarehouse}
+              onChange={(e) => setSelectedOriWarehouse(e.target.value)}
+              required
+            >
+              <option disabled value="">
+                Select Warehouse
+              </option>
+
+              {warehouseData.map((war) => (
+                <option value={war.id} key={war.id}>
+                  {war.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-3 flex items-center">
+            <label htmlFor="warehouse" className="w-5/12 text-secondary">
+              Destination Warehouse:
+            </label>
+            <select
+              name="warehouse"
+              className="select max-h-10 w-7/12 border border-tertiary bg-bgColor px-4 py-0 text-sm"
+              value={selectedDestWarehouse}
+              onChange={(e) => setSelectedDestWarehouse(e.target.value)}
               required
             >
               <option disabled value="">
@@ -68,18 +114,26 @@ function ModalMoveStockProduct({ product, onClose }) {
               ))}
             </select>
           </div>
-          <div className="flex items-center">
-            <label htmlFor="stock" className="w-5/12 text-secondary">
+          <div className="flex">
+            <label htmlFor="stock" className="w-5/12 text-secondary mt-3">
               Stock:
             </label>
-            <input
-              type="number"
-              name="stock"
-              className="input max-h-10 w-7/12 border-secondary px-4  py-0  text-sm placeholder:text-secondary"
-              value={stock}
-              onChange={(e) => setStock(e.target.value)}
-              required
-            />
+            <div className="w-7/12">
+              <input
+                type="number"
+                name="stock"
+                className="input max-h-10 w-full border-secondary px-4 py-0 text-sm placeholder:text-secondary"
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+                required
+                max={maxStock || ''}
+              />
+              {maxStock !== null && (
+                <div className="label">
+                  <span className="label-text-alt">Maximum: {maxStock}</span>
+                </div>
+              )}
+            </div>
           </div>
           <div className="container-btn-action mt-6 flex items-center justify-between">
             <button type="submit" className="btn bg-secondary text-white">
