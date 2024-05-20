24400b611a8ce9187b9ed2f8111f3a74708be96b
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-05-20 16:33:44 +0700

    fix: fix bug delete product and fix dark mode toggle

--- src/components/elements/ToggleTheme/index.jsx
+++ src/components/elements/ToggleTheme/index.jsx
@@ -6,11 +6,26 @@ import { useTheme } from '@/context/ThemeContext';
 function ToggleTheme() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className="fixed bottom-24 right-6 z-40 rounded-lg bg-secondary md:bottom-10 ">
-      <div className="flex h-8 w-8 items-center justify-center ">
+    <div>
+      <div className=" h-8 w-8 items-center justify-center hidden md:flex ">
         <button className="text-xl" onClick={toggleTheme}>
-          {theme === 'light' ? <FaMoon className="text-black" /> : <FaSun className="text-white" />}
+          {theme === 'light' ? (
+            <FaMoon className="text-tertiary" />
+          ) : (
+            <FaSun className="text-tertiary" />
+          )}
         </button>
+      </div>
+      <div className="fixed bottom-24 left-6 z-40 rounded-lg bg-secondary md:bottom-10 md:hidden ">
+        <div className="flex h-8 w-8 items-center justify-center ">
+          <button className="text-xl" onClick={toggleTheme}>
+            {theme === 'light' ? (
+              <FaMoon className="text-black" />
+            ) : (
+              <FaSun className="text-white" />
+            )}
+          </button>
+        </div>
       </div>
     </div>
   );
