86a4cc6f0b48f29bd52a9b07942f56e252ef9fea
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-05-22 19:10:28 +0700

    fix: sfixing styling list product

--- src/components/parts/ContainerProductsUser/index.jsx
+++ src/components/parts/ContainerProductsUser/index.jsx
@@ -1,14 +1,20 @@
 'use client';
 
 import CardProduct from '@/components/parts/CardProduct';
+import useSidebarStore from '@/store/sidebarStore';
 
 function ContainerProductsUser({ productsData }) {
+  const { expanded } = useSidebarStore();
   if (!productsData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="container-products mt-4 grid grid-cols-2 gap-4 p-4 md:ml-20 md:grid-cols-3 xl:grid-cols-5">
+    <div
+      className={`container-products mt-4 grid grid-cols-2 gap-4 p-4 ${
+        expanded ? 'md:ml-72 xl:grid-cols-4 ' : ' md:ml-20'
+      }  md:grid-cols-3 xl:grid-cols-5`}
+    >
       <CardProduct />
       {productsData.map((product) => (
         <CardProduct key={product.id} product={product} />
