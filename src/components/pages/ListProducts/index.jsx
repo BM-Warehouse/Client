879d5a36e080271912e0cf78ca68923f6b8930eb
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-05-22 20:48:06 +0700

    fix: fixing error push add suspense for using seacrhparams

--- src/components/pages/ListProducts/index.jsx
+++ src/components/pages/ListProducts/index.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
@@ -55,69 +55,72 @@ function ListProducts() {
   if (!role) {
     return null;
   }
+
   return (
-    <main className="product-page relative h-screen bg-bgColor font-poppins ">
-      <Navbar />
-      <Sidebar />
-      <ToggleTheme />
-      <div className="title-page flex justify-center pt-24">
-        <h1 className="text-4xl font-semibold text-tertiary xl:font-bold">Products</h1>
-      </div>
-      <div className="container-btn-products  mt-20 flex flex-col-reverse justify-between px-5 md:ml-20 md:flex-row">
-        <div className="btn-add-product">
-          {role === 'admin' ? (
-            <Link href="/add-product">
-              <button className="mt-5 min-w-28 rounded-md bg-tertiary px-3 py-2 text-primary hover:bg-secondary md:mt-0">
-                <span className="flex items-center justify-center">
-                  <HiPlus className="mr-1" />
-                  Add Product
-                </span>
-              </button>
-            </Link>
-          ) : (
-            <div> </div>
-          )}
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="product-page relative h-screen bg-bgColor font-poppins ">
+        <Navbar />
+        <Sidebar />
+        <ToggleTheme />
+        <div className="title-page flex justify-center pt-24">
+          <h1 className="text-4xl font-semibold text-tertiary xl:font-bold">Products</h1>
         </div>
-        <div className="search-filter flex items-center justify-between">
-          <label className="input  flex h-8 items-center  bg-bgColor gap-2 border-tertiary ">
-            <input
-              type="text"
-              className="grow text-sm text-tertiary transition-none placeholder:text-tertiary"
-              placeholder="Search product..."
-              value={searchKeyword}
-              onChange={onSearchKeywordChange}
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-4 w-4 text-tertiary "
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd"
+        <div className="container-btn-products mt-20 flex flex-col-reverse justify-between px-5 md:ml-20 md:flex-row">
+          <div className="btn-add-product">
+            {role === 'admin' ? (
+              <Link href="/add-product">
+                <button className="mt-5 min-w-28 rounded-md bg-tertiary px-3 py-2 text-primary hover:bg-secondary md:mt-0">
+                  <span className="flex items-center justify-center">
+                    <HiPlus className="mr-1" />
+                    Add Product
+                  </span>
+                </button>
+              </Link>
+            ) : (
+              <div> </div>
+            )}
+          </div>
+          <div className="search-filter flex items-center justify-between">
+            <label className="input flex h-8 items-center bg-bgColor gap-2 border-tertiary ">
+              <input
+                type="text"
+                className="grow text-sm text-tertiary transition-none placeholder:text-tertiary"
+                placeholder="Search product..."
+                value={searchKeyword}
+                onChange={onSearchKeywordChange}
               />
-            </svg>
-          </label>
-          <div className="btn-filter ml-5 cursor-pointer rounded-lg p-1  hover:bg-secondary">
-            <IoFilterSharp className="text-3xl text-secondary hover:text-white " />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 text-tertiary "
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
+            <div className="btn-filter ml-5 cursor-pointer rounded-lg p-1 hover:bg-secondary">
+              <IoFilterSharp className="text-3xl text-secondary hover:text-white " />
+            </div>
           </div>
         </div>
-      </div>
 
-      {role === 'admin' ? (
-        <ContainerProductsAdmin productsData={keyword ? filteredProducts : productsData} />
-      ) : (
-        <ContainerProductsUser productsData={keyword ? filteredProducts : productsData} />
-      )}
+        {role === 'admin' ? (
+          <ContainerProductsAdmin productsData={keyword ? filteredProducts : productsData} />
+        ) : (
+          <ContainerProductsUser productsData={keyword ? filteredProducts : productsData} />
+        )}
 
-      <Pagination
-        currentPage={keyword ? filteredPagination.currentPage : pagination.currentPage}
-        totalPage={keyword ? filteredPagination.totalPage : pagination.totalPage}
-        onClick={onPaginationClick}
-      />
-    </main>
+        <Pagination
+          currentPage={keyword ? filteredPagination.currentPage : pagination.currentPage}
+          totalPage={keyword ? filteredPagination.totalPage : pagination.totalPage}
+          onClick={onPaginationClick}
+        />
+      </main>
+    </Suspense>
   );
 }
 
