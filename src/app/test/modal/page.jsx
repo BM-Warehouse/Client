4614a1b6a407d4637cc69e16c94065fe23b7e793
Yohan Prakoso <prakoso.yohan@gmail.com>
2024-05-22 11:53:06 +0800

    fix: lint error

--- src/app/test/modal/page.jsx
+++ src/app/test/modal/page.jsx
@@ -68,7 +68,82 @@ const Modal1 = () => {
   };
 
   return (
-    <Modal action={serverHandleSubmit} id={modalId} title="Modal 1" onSubmit={closeModal}>
+    <Modal id={modalId} title="Modal 1" onSubmit={handleSubmit} description='This is Modal 1'>
+      <div className="overflow-y-auto max-h-96 w-full p-1 scrollbar-hide">
+        <Input
+          label="Input 1"
+          name="input1"
+          value={in1}
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <Input
+          label="Input 2"
+          name="input2"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <TextArea
+          label="Text Area 1"
+          name="textArea1"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <TextArea
+          label="Text Area 2"
+          name="textArea2"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <Select label="Select 1" name="select1">
+          <option value="option 1">option 1</option>
+          <option value="option 2">option 2</option>
+          <option value="option 3">option 3</option>
+        </Select>
+        <Select label="Select 2" name="select2">
+          <option value="option 1">option 1</option>
+          <option value="option 2">option 2</option>
+          <option value="option 3">option 3</option>
+        </Select>
+        <InputFile label="Input File" name="file1" onChange={handleChange} />
+      </div>
+      <div className="flex items-center justify-between w-full mt-4">
+        <ButtonPrimary type="submit">Submit</ButtonPrimary>
+        <ButtonStrong type="button" onClick={closeModal}>
+          Cancel
+        </ButtonStrong>
+      </div>
+    </Modal>
+  );
+};
+
+const Modal2 = () => {
+  const modalId = 'modal-test-2';
+  const [in1, setIn1] = useState('default in 1');
+
+  // kalo butuh aksi saat nilai berubah,
+  // biasanya dipakek klo valuenya refer ke suatu state
+  const handleChange = (e) => {
+    if (e.target.name === 'input1') setIn1(e.target.value);
+    // else if(e.target.name === 'file1') console.log(e.target.files) // kalo mau lihat file change
+  };
+
+  const closeModal = () => {
+    closeModalWithId(modalId);
+  };
+
+  return (
+    <Modal
+      action={serverHandleSubmit}
+      id={modalId}
+      title="Modal 2"
+      onSubmit={closeModal}
+      description='This is Modal 2'
+    >
       <div className="overflow-y-auto max-h-96 w-full p-1 scrollbar-hide">
         <Input
           label="Input 1"
@@ -135,8 +210,17 @@ const test = () => (
         >
           Open modal 1
         </ButtonPrimary>
+        <ButtonPrimary
+          icon="chevronR"
+          onClick={() => {
+            openModalWithId('modal-test-2');
+          }}
+        >
+          Open modal 2
+        </ButtonPrimary>
       </div>
       <Modal1 />
+      <Modal2 />
     </ContentContainer>
   </div>
 );
