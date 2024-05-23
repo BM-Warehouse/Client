28c9f8ad187feae478288b10c536161419eff127
AlexWiraWilantapoera <alexwira99@gmail.com>
2024-05-23 23:51:13 +0700

    feat: tambah filter untuk kategori

--- src/components/pages/ListCategories/index.jsx
+++ src/components/pages/ListCategories/index.jsx
@@ -9,7 +9,6 @@ import { IoFilterSharp } from 'react-icons/io5';
 
 import ContainerCategory from '@/components/parts/ContainerCategory';
 import Pagination from '@/components/parts/Pagination';
-import useInput from '@/hooks/useInput';
 import useAuthUserStore from '@/store/authUserStore';
 import useCategoryStore from '@/store/categoryStore';
 import useProductStore from '@/store/productStore';
@@ -17,13 +16,14 @@ import useProductStore from '@/store/productStore';
 function ListCategories() {
   const { role } = useAuthUserStore();
 
-  const [name, onNameChange] = useInput('');
-  const [description, onDescriptionChange] = useInput('');
+  const [name, setName] = useState('');
+  const [orderBy, setOrderBy] = useState('');
+  const [orderType, setOrderType] = useState('');
+  const [description, setDescription] = useState('');
   const [file, setFile] = useState(null);
-
+  const [isLoading, setLoading] = useState(true);
   const { categoriesData, asyncGetAllCategory, asyncAddCategory, pagination } = useCategoryStore();
   const { productsData } = useProductStore();
-  const [isLoading, setLoading] = useState(true);
   let tSearch = null;
 
   useEffect(() => {
@@ -37,7 +37,7 @@ function ListCategories() {
   }
 
   const onPaginationClick = async (page) => {
-    await asyncGetAllCategory(page);
+    await asyncGetAllCategory('', page);
   };
 
   const handleFileChange = (e) => {
@@ -51,12 +51,29 @@ function ListCategories() {
     }, 1000);
   };
 
+  const onCloseAddModal = () => {
+    document.getElementById('modal_add_category').close();
+    setName('');
+    setDescription('');
+  };
+
+  const onCloseFilterModal = () => {
+    document.getElementById('modal_filter_category').close();
+  };
+
+  const handleApplyFilter = async (e) => {
+    e.preventDefault();
+    console.log(orderBy, orderType);
+    await asyncGetAllCategory('', 1, 10, orderBy, orderType);
+    onCloseFilterModal();
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!file) {
       // eslint-disable-next-line no-alert
-      alert('Please select a file to upload.');
+      toast('Please select a file to upload.');
       return;
     }
 
@@ -78,7 +95,9 @@ function ListCategories() {
       const newCategory = await asyncAddCategory({ name, description, imageUrl });
       if (newCategory) {
         toast.success('Category added successfully!');
-        document.getElementById('my_modal_1').close();
+        setName('');
+        setDescription('');
+        document.getElementById('modal_add_category').close();
         await asyncGetAllCategory();
       }
     } catch (error) {
@@ -107,63 +126,66 @@ function ListCategories() {
             <>
               <button
                 className="btn bg-secondary text-white"
-                onClick={() => document.getElementById('my_modal_1').showModal()}
+                onClick={() => document.getElementById('modal_add_category').showModal()}
               >
                 Add Category
               </button>
-              <dialog id="my_modal_1" className="modal">
+              <dialog id="modal_add_category" className="modal">
                 <div className="modal-box bg-primary ">
                   <h3 className="text-lg font-bold text-secondary">Add New Category</h3>
-                  <form onSubmit={handleSubmit}>
-                    <div className="form-control">
-                      <label htmlFor="name" className="label text-secondary">
-                        Category name:
-                      </label>
-                      <input
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="Enter new name..."
-                        onChange={onNameChange}
-                        value={name}
-                        name="name"
-                      />
+                  <div className="form-control">
+                    <label htmlFor="name" className="label text-secondary">
+                      Category name:
+                    </label>
+                    <input
+                      className="input input-bordered"
+                      type="text"
+                      placeholder="Enter new name..."
+                      onChange={(e) => setName(e.target.value)}
+                      value={name}
+                      name="name"
+                    />
+                  </div>
+                  <div className="form-control">
+                    <label htmlFor="" className="label text-secondary">
+                      Description:
+                    </label>
+                    <input
+                      className="input input-bordered"
+                      type="text"
+                      placeholder="Enter description..."
+                      onChange={(e) => setDescription(e.target.value)}
+                      value={description}
+                      name="description"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="file"
+                      onChange={handleFileChange}
+                      className="file-input file-input-bordered file-input-sm mt-5 w-full max-w-xs text-secondary file:border-secondary file:bg-secondary file:text-white"
+                    />
+                  </div>
+                  <div className="container-btn-action flex items-center justify-between">
+                    <div className="container-btn-submit mt-7">
+                      <button
+                        type="submit"
+                        onClick={handleSubmit}
+                        className="btn text-white bg-secondary"
+                      >
+                        Submit
+                      </button>
                     </div>
-                    <div className="form-control">
-                      <label htmlFor="" className="label text-secondary">
-                        Description:
-                      </label>
-                      <input
-                        className="input input-bordered"
-                        type="text"
-                        placeholder="Enter description..."
-                        onChange={onDescriptionChange}
-                        value={description}
-                        name="description"
-                      />
+                    <div className="modal-action">
+                      <button
+                        type="button"
+                        className="btn bg-secondary text-white"
+                        onClick={onCloseAddModal}
+                      >
+                        Close
+                      </button>
                     </div>
-                    <div>
-                      <input
-                        type="file"
-                        onChange={handleFileChange}
-                        className="file-input file-input-bordered file-input-sm mt-5 w-full max-w-xs text-secondary file:border-secondary file:bg-secondary file:text-white"
-                      />
-                    </div>
-                    <div className="container-btn-action flex items-center justify-between">
-                      <div className="container-btn-submit mt-7">
-                        <button type="submit" className="btn text-white bg-secondary">
-                          Submit
-                        </button>
-                      </div>
-                      <div className="modal-action">
-                        <button
-                          className="btn bg-secondary text-white"
-                          onClick={() => document.getElementById('my_modal_1').close()}
-                        >
-                          Close
-                        </button>
-                      </div>
-                    </div>
-                  </form>
+                  </div>
                 </div>
               </dialog>
             </>
@@ -193,8 +215,57 @@ function ListCategories() {
             </svg>
           </label>
           <div className="btn-filter ml-5 cursor-pointer rounded-lg p-1 hover:bg-secondary">
-            <IoFilterSharp className="text-3xl text-secondary hover:text-white" />
+            <button onClick={() => document.getElementById('modal_filter_category').showModal()}>
+              <IoFilterSharp className="text-3xl text-secondary hover:text-white" />
+            </button>
           </div>
+          <dialog id="modal_filter_category" className="modal">
+            <div className="modal-box bg-primary">
+              <h3 className="font-bold text-lg text-tertiary">Filter Category</h3>
+              <div className="form-contro my-5">
+                <label htmlFor="orderBy">Order by:</label>
+                <select
+                  name="orderBy"
+                  onChange={(e) => setOrderBy(e.target.value)}
+                  className="border-2 w-full rounded-md h-10"
+                >
+                  <option value="id">Id</option>
+                  <option value="name">Name</option>
+                </select>
+              </div>
+              <div className="form-control">
+                <label htmlFor="orderType">Order type:</label>
+                <select
+                  name="orederType"
+                  onChange={(e) => setOrderType(e.target.value)}
+                  className="border-2 w-full rounded-md h-10"
+                >
+                  <option value="asc">Ascending</option>
+                  <option value="desc">Descending</option>
+                </select>
+              </div>
+              <div className="container-btn-action flex items-center justify-between">
+                <div className="container-btn-submit mt-7">
+                  <button
+                    type="submit"
+                    onClick={handleApplyFilter}
+                    className="btn text-white bg-secondary"
+                  >
+                    Apply
+                  </button>
+                </div>
+                <div className="modal-action">
+                  <button
+                    type="button"
+                    className="btn bg-secondary text-white"
+                    onClick={onCloseFilterModal}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </dialog>
         </div>
       </div>
       {/* <div className="container-products mt-24 grid grid-cols-2 gap-4 p-4 md:ml-20 md:grid-cols-3 xl:grid-cols-5"> */}
