d3a238f0c470fe047a4990ef39ea1459f623f12f
Yuma Nur Alfath <108206424+yumanuralfath@users.noreply.github.com>
2024-05-23 08:51:05 +0700

    feat: no login push to not found

--- src/components/pages/Dashboard/index.jsx
+++ src/components/pages/Dashboard/index.jsx
@@ -1,11 +1,19 @@
 'use client';
 
 // import CheckoutInfo from './components/CheckoutInfo';
+import NotFound from '@/app/not-found';
+import useAuthUserStore from '@/store/authUserStore';
+
 import CheckoutInfoButton from './components/CheckoutInfo';
 import AreaChart from './components/ProfitChart';
 import QuantitiesChart from './components/QuantitiesChart';
 
 function Dashboard() {
+  const { role } = useAuthUserStore();
+  if (!role) {
+    return <NotFound />;
+  }
+
   return (
     <main className="dashboard-admin relative h-screen bg-bgColor font-poppins px-4 m:flex sm:justify-between sm:items-center mb-8">
       <div className="title-page flex justify-center pt-20 mb-8">
