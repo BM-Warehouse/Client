0b8be91961c41189d9b4de267340f24a94abf756
Yohan Prakoso <prakoso.yohan@gmail.com>
2024-05-25 05:11:26 +0800

    fix: lint

--- src/app/test/squareImage/page.js
+++ src/app/test/squareImage/page.js
@@ -1,12 +1,11 @@
-import Image from 'next/image'
-import React from 'react'
+import React from 'react';
 
-const page = () => {
-  return (
-    <div>
-      <Image src={"https://res.cloudinary.com/denyah3ls/image/upload/v1716386344/mifwrz1trf113jk6v1ij.jpg"} width={500} height={50} className='w-96 h-full'/>
-    </div>
-  )
-}
+import Image from 'next/image';
 
-export default page
+const page = () => (
+  <div>
+    <Image src="https://res.cloudinary.com/denyah3ls/image/upload/v1716386344/mifwrz1trf113jk6v1ij.jpg" width={500} height={50} className='w-96 h-full'/>
+  </div>
+  );
+
+export default page;
