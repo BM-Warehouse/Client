a31fbfcd35742035889f4908ec7b6217de7f177a
Yohan Prakoso <prakoso.yohan@gmail.com>
2024-05-26 03:01:57 +0800

    feat: update ModalEditCategory2

--- src/components/pages/DetailCategory/ModalEditCategory.jsx
+++ src/components/pages/DetailCategory/ModalEditCategory.jsx
@@ -6,6 +6,7 @@ import toast from 'react-hot-toast';
 
 import { ButtonPrimary } from '@/components/parts/Button';
 import { Input, InputFile, Modal, TextArea } from '@/components/parts/Modal';
+import { uploadV1 } from '@/lib/upload';
 import useCategoryStore from '@/store/categoryStore';
 
 const modalId = 'modal-edit-category';
@@ -149,72 +150,35 @@ const ModalEditCategory2 = ({ id }) => {
     })
   );
 
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [file, setFile] = useState(null);
-
-  useEffect(() => {
-    if (categoryDetail) {
-      setName(categoryDetail.name);
-      setDescription(categoryDetail.description);
-    }
-  }, [categoryDetail]);
-
-  const handleChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    let imageUrl;
     try {
-      let imageUrl = null;
-      if (file) {
-        const formData = new FormData();
-        formData.append('file', file);
-        formData.append('upload_preset', 'rwheysjo');
-        const response = await fetch('https://api.cloudinary.com/v1_1/denyah3ls/image/upload', {
-          method: 'POST',
-          body: formData
-        });
-        // secureUrl ganti dulu jadi secure_url
+      const form = new FormData(e.target);
+      const categoryName = form.get('categoryName');
+      const description = form.get('description');
+      const image = form.get('image');
+      if (image.size) {
         // eslint-disable-next-line camelcase
-        const { secure_url } = await response.json();
+        const { secure_url } = await uploadV1(image);
         // eslint-disable-next-line camelcase
         imageUrl = secure_url;
       }
-
-      // eslint-disable-next-line camelcase
       imageUrl = imageUrl || categoryDetail.imageUrl;
-
-      // if (name === '' || description === '') {
-      //   setName(categoryDetail.name);
-      //   setDescription(categoryDetail.description);
-      // }
-
-      // console.log(id, { name, description, imageUrl });
-      await asyncEditCategory(id, { name, description, imageUrl });
-      closeModalEditCategory();
-      toast.success('Category updated successfully!');
+      await asyncEditCategory(id, { categoryName, description, imageUrl });
+      toast.success('Category Updated Successfully!');
       await asyncGetDetailCategory(id);
-    } catch (error) {
-      console.log(`category edited failed: ${error}`);
+      closeModalEditCategory();
+    } catch (e) {
+      toast.error('Something Wrong');
     }
   };
 
   return (
     <Modal id={modalId} title="Edit Category" onSubmit={handleSubmit}>
-      <Input
-        label="Category Name"
-        defaultValue={categoryDetail.name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <TextArea
-        label="Description"
-        defaultValue={categoryDetail.description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <InputFile label="Category Picture" onChange={handleChange} />
+      <Input label="Category Name" defaultValue={categoryDetail.name} name="categoryName" />
+      <TextArea label="Description" defaultValue={categoryDetail.description} name="description" />
+      <InputFile label="Category Picture" name="image" />
       <div className="flex justify-center w-full">
         <ButtonPrimary className="mr-5" type="submit">
           Submit
