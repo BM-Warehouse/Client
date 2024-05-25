e7a5626c71332c0275f276f7ce3abfd51ccd86b4
Yohan Prakoso <prakoso.yohan@gmail.com>
2024-05-26 04:27:06 +0800

    feat: ubah posisi category

--- src/components/parts/ContainerProductCategory/index.jsx
+++ src/components/parts/ContainerProductCategory/index.jsx
@@ -8,7 +8,7 @@ function ContainerProductCategory({ productCategories }) {
   }
 
   return (
-    <div className="container-products mt-4 grid grid-cols-2 gap-4 p-4 md:ml-20 md:grid-cols-3 xl:grid-cols-5">
+    <div className="container-products mt-4 grid grid-cols-2 gap-4 p-4 md:ml-20 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {productCategories.map((productCategory) => (
         <CardProduct key={productCategory.product.id} product={productCategory.product} />
       ))}
